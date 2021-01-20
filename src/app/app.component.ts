8fe3c5c4657bfd424e5b858944b5ddd5d59d1524
marcsim <marc-antoine.simon3@orange.fr>
2021-01-20 23:24:21 +0100

    work in progress : counter with observable + subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'projetTest';
+  public secondes: number;
+  counterSubscription: Subscription;
+
+  ngOnInit() {
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log("We have an error " + error);
+      },
+      () => {
+        console.log("Observable complete");
+      } 
+    )
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
 }
